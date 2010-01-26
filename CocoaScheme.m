e3ddd56db0f77d59b0eb2cbe34f4a148835bd2b4
Dmitry Chestnykh <dmitry@codingrobots.com>
2010-01-26 22:00:46 +0300

    Multiline REPL.

--- CocoaScheme.m
+++ CocoaScheme.m
@@ -20,24 +20,38 @@ int main (int argc, const char *argv[])
     [sc loadFile:[NSString stringWithUTF8String:argv[1]]];
   } else {
     // mini REPL
-    printf("CocoaScheme. Press Ctrl-C to exit.");
+    printf("CocoaScheme. Press Ctrl-C to exit.\n");
     // prepare some helpers
     [sc evalString:@"(defmacro class (name)"
                     " `(string->objc:class (symbol->string ',name)))"];
     
     char buffer[512];
-    char response[1024];
+    NSMutableString *s = [[NSMutableString alloc] init];
     while (1)                           /* fire up a REPL */
     {
-      fprintf(stdout, "\n> ");        /* prompt for input */
+      fprintf(stdout, "> ");        /* prompt for input */
       fgets(buffer, 512, stdin);
       if (feof(stdin))
         return 0;
-      if ((buffer[0] != '\n') || 
-          (strlen(buffer) > 1))
-      {
-        sprintf(response, "(write %s)", buffer);
-        [sc evalString:[NSString stringWithUTF8String:response]]; /* evaluate input and write the result */
+      [s appendFormat:@"%s", buffer];
+      // count parentheses
+      int parentheses = 0;
+      for (int i = 0; i < [s length]; i++) {
+        unichar c = [s characterAtIndex:i];
+        if (c == '(')
+          parentheses++;
+        else if (c == ')')
+          parentheses--;
+      }
+      if (parentheses > 0) {
+        printf(">");
+        continue;
+      }
+      
+      if ([s length] > 0 && [s characterAtIndex:0] != '\n') {
+        [sc evalString:[NSString stringWithFormat:@"(write %@)", s]];
+        [s setString:@""];
+        printf("\n");
       }
     }
   }
